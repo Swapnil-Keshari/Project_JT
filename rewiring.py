9af8bcd9118238e7e2a7052c56c931c946dd4c15
Swapnil-Keshari <64347268+Swapnil-Keshari@users.noreply.github.com>
2020-06-13 14:40:00 +0530

    Update rewiring.py
    Adding comments

--- rewiring.py
+++ rewiring.py
@@ -2,7 +2,9 @@
 """
 Created on Thu May 14 10:28:45 2020
 
-@author: 98kes
+@author: Swapnil Keshari
+
+Summary: This file rewires the network form the Dict 2 which is inporeted form the earlier file
 """
 
 import networkx as nx
@@ -12,7 +14,7 @@ G = nx.DiGraph()
 
 Gene = Dict2.keys()
 G.add_nodes_from(Gene)
-
+#Creaitng an initial network for testing
 for index in range(len(Gene)):
     if index != len(Gene)-1:
         G.add_edges_from([(Gene[index],Gene[index+1])],signal='i')
@@ -20,49 +22,25 @@ for index in range(len(Gene)):
 #        G.add_edges_from([(Gene[index],Gene[0])],signal='i')
 
 nx.write_gml(G, "test.gml") 
-       
-#G.add_edges_from([(1,3)],signal='i')
-#G.add_edges_from([(2,3),(3,4)],signal='a')
-#nx.write_gml(G, "test.gml")
-
-#GeneDict={1:None,2:["3P","6P","2P","3P-6P","3P-6P-2P"],3:["4P","5P","3P-4P"],4:None}
 
 removeNodeList=[]
-
+#Creating a list of gene which has phosporylation position attached to it
 for gene in Dict2.keys():
     for seq in Dict2[gene].keys():
         if Dict2[gene][seq].keys() != None:
             removeNodeList.append(gene)
             break
-
-#for rm in removeNodeList:
-#    for value in GeneDict[rm]:
-#        if '-' not in value:
-#            for start in G.predecessors(rm):
-#                edge1=G.get_edge_data(start,rm)["signal"]
-#                if edge1=="i":
-#                    G.add_edge(start,value,signal="i")
-#                else:
-#                    G.add_edge(start,value,signal="a")
-#            for finish in G.successors(rm):
-#                edge2=G.get_edge_data(rm,finish)["signal"]
-#                if edge2=="i":
-#                    G.add_edge(value,finish,signal="i")
-#                else:
-#                    G.add_edge(value,finish,signal="a")
-
-
-
-
+#Rewiring program starts
 for gene in removeNodeList:
     print gene
+    #All the different sequence will be in parallel to each other
     for seq in Dict2[gene].keys():
         print seq
-        monoList=[]
-        diList=[]
-        triList=[]
-        removeDiList=[]
-        removeTriList=[]
+        monoList=[]#Used to store monophosphorylations
+        diList=[]#Used to store diphosphorylations
+        triList=[]#Used to store triphosphorylations
+        removeDiList=[]#Used to store diphosphorylations node which will be removed from diList
+        removeTriList=[]#Used to store monophosphorylations which will be removed from triList
         for phospho in Dict2[gene][seq].keys():
             if '1x' in phospho:
                 monoList.append(str(gene)+"_"+str(seq)+"_"+str(phospho))
@@ -73,7 +51,9 @@ for gene in removeNodeList:
         G.add_nodes_from(monoList)
         G.add_nodes_from(diList)
         G.add_nodes_from(triList)
+        #Checking if monophosphorylations exist
         if monoList !=[]:
+            #All mono are attached to the source nodes
             for mono in monoList:
                 for start in G.predecessors(gene):
                     edge1=G.get_edge_data(start,gene)["signal"]
@@ -90,7 +70,7 @@ for gene in removeNodeList:
                     for di in diList:
                         new2=di.split('_2x')
                         v2=set(new2[1].split('-'))
-                        if v1.issubset(v2):
+                        if v1.issubset(v2):#Checks if the mono are present in di or not
                             G.add_edge(mono,di,signal="a")
                             isSubsetofDi=isSubsetofDi+1
                             if triList!=[]:
@@ -98,7 +78,7 @@ for gene in removeNodeList:
                                 for tri in triList:
                                     new3=tri.split('_3x')
                                     v3=set(new3[1].split('-'))
-                                    if v2.issubset(v3):
+                                    if v2.issubset(v3):#Checks if the same di is present in the tri
                                         G.add_edge(di,tri,signal="a")
                                         isSubsetofTri=isSubsetofTri+1
                                         for finish in G.successors(gene):
@@ -108,29 +88,29 @@ for gene in removeNodeList:
                                             else:
                                                 G.add_edge(tri,finish,signal="a")
                                         removeTriList.append(tri)
-                                if isSubsetofTri==0:
+                                if isSubsetofTri==0:#If di is not the subset of tri we join it to finish
                                     for finish in G.successors(gene):
                                         edge2=G.get_edge_data(gene,finish)["signal"]
                                         if edge2=="i":
                                             G.add_edge(di,finish,signal="i")
                                         else:
                                             G.add_edge(di,finish,signal="a")
-                                    removeDiList.append(di)
-                            else:
+                                    removeDiList.append(di)#Add it to the remove node list so that is not added to source later
+                            else:#If there is no tri list
                                 for finish in G.successors(gene):
                                     edge2=G.get_edge_data(gene,finish)["signal"]
                                     if edge2=="i":
                                         G.add_edge(di,finish,signal="i")
                                     else:
                                         G.add_edge(di,finish,signal="a")
-                                removeDiList.append(di)
-                    if isSubsetofDi==0:
-                        if triList!=[]:
+                                removeDiList.append(di)#Add it to the remove node list so that is not added to source later
+                    if isSubsetofDi==0:#Even though diList is there but mono isn't the subset of it
+                        if triList!=[]:#We check that is it present in tri list
                             isSubsetofTri=0
                             for tri in triList:
                                 new3=tri.split('_3x')
                                 v3=set(new3[1].split('-'))
-                                if v1.issubset(v3):
+                                if v1.issubset(v3):#If mono is the subset of tri
                                     G.add_edge(mono,tri,signal="a")
                                     isSubsetofTri=isSubsetofTri+1
                                     for finish in G.successors(gene):
@@ -139,27 +119,27 @@ for gene in removeNodeList:
                                             G.add_edge(tri,finish,signal="i")
                                         else:
                                             G.add_edge(tri,finish,signal="a")
-                                    removeTriList.append(tri)
-                            if isSubsetofTri==0:
+                                    removeTriList.append(tri)#Add it to the remove node list so that is not added to source later
+                            if isSubsetofTri==0:#Even if tri is present but mono is not the subset of tri
                                 for finish in G.successors(gene):
                                     edge2=G.get_edge_data(gene,finish)["signal"]
                                     if edge2=="i":
                                         G.add_edge(mono,finish,signal="i")
                                     else:
                                         G.add_edge(mono,finish,signal="a")
-                        else:
+                        else:#if it is not in the subset of di but tri doen't exist
                             for finish in G.successors(gene):
                                 edge2=G.get_edge_data(gene,finish)["signal"]
                                 if edge2=="i":
                                     G.add_edge(mono,finish,signal="i")
                                 else:
                                     G.add_edge(mono,finish,signal="a")                                    
-                elif triList!=[]:
+                elif triList!=[]:#if there is no di list but tri list exist
                     isSubsetofTri=0
                     for tri in triList:
                         new3=tri.split('_3x')
                         v3=set(new3[1].split('-'))
-                        if v1.issubset(v3):
+                        if v1.issubset(v3):#If mono is in the subset of tri
                             G.add_edge(mono,tri,signal="a")
                             isSubsetofTri=isSubsetofTri+1
                             for finish in G.successors(gene):
@@ -168,23 +148,24 @@ for gene in removeNodeList:
                                     G.add_edge(tri,finish,signal="i")
                                 else:
                                     G.add_edge(tri,finish,signal="a")
-                            removeTriList.append(tri)
-                    if isSubsetofTri==0:
+                            removeTriList.append(tri)#Add it to the remove node list so that is not added to source later
+                    if isSubsetofTri==0:#Even if tri exist but mono isn't in the subset of tri
                         for finish in G.successors(gene):
                             edge2=G.get_edge_data(gene,finish)["signal"]
                             if edge2=="i":
                                 G.add_edge(mono,finish,signal="i")
                             else:
                                 G.add_edge(mono,finish,signal="a")
-                else:
+                else:#Only mono list exist
                     for finish in G.successors(gene):
                         edge2=G.get_edge_data(gene,finish)["signal"]
                         if edge2=="i":
                             G.add_edge(mono,finish,signal="i")
                         else:
                             G.add_edge(mono,finish,signal="a")
-            diList=[x for x in diList if x not in removeDiList]  
-        elif diList!=[]:
+            diList=[x for x in diList if x not in removeDiList] #Remove all the diList elements which have mono as source 
+        #if no monophosphrylation then checking existance of diphosphorylation
+        elif diList!=[]:#Elements which don't have mono as the source present
             for di in diList:
                 for start in G.predecessors(gene):
                     edge1=G.get_edge_data(start,gene)["signal"]
@@ -194,12 +175,12 @@ for gene in removeNodeList:
                         G.add_edge(start,di,signal="a")
                 new2=di.split('_2x')
                 v2=set(new2[1].split('-'))
-                if triList!=[]:
+                if triList!=[]:#If trilist exist
                     isSubsetofTri=0
                     for tri in triList:
                         new3=tri.split('_3x')
                         v3=set(new3[1].split('-'))
-                        if v2.issubset(v3):
+                        if v2.issubset(v3):#If di exist in tri
                             G.add_edge(di,tri,signal="a")
                             isSubsetofTri=isSubsetofTri+1
                             for finish in G.successors(gene):
@@ -208,23 +189,24 @@ for gene in removeNodeList:
                                     G.add_edge(tri,finish,signal="i")
                                 else:
                                     G.add_edge(tri,finish,signal="a")
-                            removeTriList.append(tri)
-                    if isSubsetofTri==0:
+                            removeTriList.append(tri)#Add it to the remove node list so that is not added to source later
+                    if isSubsetofTri==0:#Even though tri exist but still it is not in any tri
                         for finish in G.successors(gene):
                             edge2=G.get_edge_data(gene,finish)["signal"]
                             if edge2=="i":
                                 G.add_edge(mono,finish,signal="i")
                             else:
                                 G.add_edge(mono,finish,signal="a")                                
-                else:
+                else:#If tri doesn't exist
                     for finish in G.successors(gene):
                         edge2=G.get_edge_data(gene,finish)["signal"]
                         if edge2=="i":
                             G.add_edge(di,finish,signal="i")
                         else:
                             G.add_edge(di,finish,signal="a")        
-            triList=[x for x in triList if x not in removeTriList]             
-        elif triList!=[]:
+            triList=[x for x in triList if x not in removeTriList]   #Remove all the trList elements which have mono or di as source           
+        #Else triphosphorylation shall exist since we have some type of phosphorylation attached
+        elif triList!=[]:#Elements which donot have and di or mono as source
             for tri in triList:
                 for start in G.predecessors(gene):
                     edge1=G.get_edge_data(start,gene)["signal"]
@@ -241,75 +223,3 @@ for gene in removeNodeList:
 
 G.remove_nodes_from(removeNodeList)                        
 nx.write_gml(G, "test1.gml")
-#nx.draw(G,with_labels=True)
-#plt.savefig("graph.png")
-#plt.show()
-
-
-
-
-
-
-#    if addNodeList != []:
-#        for add in addNodeList:
-#            for element in add.split('-'):
-#                G.add_edge(element,add,signal="a")
-#                for start in G.predecessors(rm):
-#                    edge1=G.get_edge_data(start,rm)["signal"]
-#                    if edge1=="i":
-#                        G.add_edge(start,element,signal="i")
-#                    else:
-#                        G.add_edge(start,element,signal="a")
-#            for finish in G.successors(rm):
-#                edge2=G.get_edge_data(rm,finish)["signal"]
-#                if edge2=="i":
-#                    G.add_edge(add,finish,signal="i")
-#                else:
-#                    G.add_edge(add,finish,signal="a")
-#    else:
-#        for value in GeneDict[rm]:
-#            if value not in addNodeList:
-#                for start in G.predecessors(rm):
-#                    edge1=G.get_edge_data(start,rm)["signal"]
-#                    if edge1=="i":
-#                        G.add_edge(start,value,signal="i")
-#                    else:
-#                        G.add_edge(start,value,signal="a")
-#                for finish in G.successors(rm):
-#                    edge2=G.get_edge_data(rm,finish)["signal"]
-#                    if edge2=="i":
-#                        G.add_edge(value,finish,signal="i")
-#                    else:
-#                        G.add_edge(value,finish,signal="a")
-#            
-#
-#G.remove_nodes_from(removeNodeList)
-#
-#nx.write_gml(G, "test1.gml")
-
-
-
-
-
-
-
-                        
-#for rm in removeNodeList:
-#	for start in G.predecessors(rm):
-#		edge1=G.get_edge_data(start,rm)['signal']
-#		if edge1=='i':
-#			for element in rm.split('-'):
-#				G.add_edge(start,element,signal='i')
-#		else:
-#			for element in rm.split('-'):
-#				G.add_edge(start,element,signal='a')
-#                
-#	for finish in G.successors(rm):
-#		edge2=G.get_edge_data(rm,finish)['signal']		
-#		if edge2=='i':
-#			for element in rm.split('-'):
-#				G.add_edge(element,finish,signal='i')
-#		else:
-#			for element in rm.split('-'):
-#				G.add_edge(element,finish,signal='a')
-#	G.remove_node(rm)
