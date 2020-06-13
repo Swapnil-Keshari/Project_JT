ff1ed9dd2c21281a57e1cf2262998bc66c3f25b8
Swapnil-Keshari <64347268+Swapnil-Keshari@users.noreply.github.com>
2020-06-13 12:03:14 +0530

    Add files via upload

--- dataprocessing.py
+++ dataprocessing.py
@@ -2,23 +2,24 @@
 """
 Created on Thu May 14 11:18:21 2020
 
-@author: 98kes
+@author: Swapnil Keshari
+
+Summary: This file reads the data form the csv file and operates on it to create
+another dataframe which has unique rows and Phospho as columns
 """
 
 import pandas as pd
-"""
-Adding comments
-"""
+
 ph = pd.read_csv('D:\Desktop\IITB\Project_Juilee\phosphoproteomics.csv', nrows=50)
 
+#Replacing all the NA with '0'
 ph.fillna(0, inplace=True)  
 ph.sort_values(by=['Gene','Sequence'],inplace=True)
 ph.reset_index(drop=True, inplace=True)
 ph.drop(columns = "Unnamed: 0", inplace = True)
 
-
 #ph.dropna(inplace = True) 
-
+#Modifying the 'Modification' and extracting Phophorylation sites from it using string manipulations
 new= ph["Modifications"].str.split(' (?=\dxPhospho)', n = 1, expand = True)
 ph["Phospho"] = new[1]
 ph['Phospho'] = ph["Phospho"].astype(str)
@@ -28,10 +29,11 @@ ph['Phospho'] = ph["Phospho"].str.replace('T/S', 'S/T')
 ph.drop(columns =["Modifications"], inplace = True)
 
 #ph['Sequence1'] = ph[['Gene', 'Sequence', 'Phospho']].agg('-'.join, axis=1)
+#Creating a Sequence1 which is a unique identifier for each modification
 ph['Sequence1']=ph['Gene'].astype(str)+"_"+ph['Sequence'].astype(str)+"_"+ph['Phospho'].astype(str)
-    
+#Grouping all the non unique entries and adding the values corresponding to it    
 res = ph.groupby('Sequence1', as_index=False).sum()
-
+#Creating  new dataframe having separate columns for Gene Sequence and Phospho
 new= res["Sequence1"].str.split('_', n = 2, expand = True)
 res["Gene"] = new[0].astype(str)
 res["Sequence"]=new[1].astype(str)
